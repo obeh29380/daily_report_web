ace8124b82fe4a9068ccb09250ff52b40798a5c4
root <root@RYUPC>
2024-02-23 20:26:03 +0900

    fix: Fix selection of invalid docker compose version

--- init_docker.sh
+++ init_docker.sh
@@ -5,7 +5,7 @@ sudo systemctl enable docker
 
 sudo mkdir -p /usr/local/lib/docker/cli-plugins
 cd /usr/local/lib/docker/cli-plugins
-sudo curl -OL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-aarch64
-sudo mv docker-compose-linux-aarch64 docker-compose
+sudo curl -OL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64
+sudo mv docker-compose-linux-x86_64 docker-compose
 sudo chmod +x docker-compose
 sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
