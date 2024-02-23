2c31ed0d84e769ce24902d1975aebb4b7e458ab0
root <root@RYUPC>
2024-02-23 20:16:24 +0900

    fix: Fix wrong sym link path

--- init_docker.sh
+++ init_docker.sh
@@ -8,4 +8,4 @@ cd /usr/local/lib/docker/cli-plugins
 sudo curl -OL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-aarch64
 sudo mv docker-compose-linux-aarch64 docker-compose
 sudo chmod +x docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
