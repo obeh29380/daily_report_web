842a1a5629457102c07f374c9ec630f658d6fb30
root <root@ip-172-31-14-64.ap-northeast-1.compute.internal>
2024-02-23 12:03:44 +0000

    fix: Change default file permission

--- init_docker.sh
+++ init_docker.sh
@@ -8,4 +8,5 @@ cd /usr/local/lib/docker/cli-plugins
 sudo curl -OL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64
 sudo mv docker-compose-linux-x86_64 docker-compose
 sudo chmod +x docker-compose
+sudo chown root:docker docker-compose
 sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
